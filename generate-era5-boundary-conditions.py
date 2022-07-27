bc298cb37883b2eed6f76f3d26da5de24abab9c4
Aaron Alexander <aaron.alexander@jupiterintel.com>
2022-07-27 18:40:04 +0000

    change divisor for rain to be correct

--- generate-era5-boundary-conditions.py
+++ generate-era5-boundary-conditions.py
@@ -263,7 +263,7 @@ def run(start_date, end_date, freq_want, save_location, geo_file):
                     if (output_name == 'SWDOWN'):
                         divisor = 3600
                     elif (output_name == 'RAINRATE'):
-                        divisor = 1/3600 #rainfall is in m 
+                        divisor = 3600/1000 #rainfall is in m and this is a divisor, so flip the 1000/3600 that we would expect
                     else:
                         divisor = 1
 
